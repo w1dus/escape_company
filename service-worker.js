58a60ea76393f6f89d0940dbf819c3815f941790
Kim ji yeon <67830760+w1dus@users.noreply.github.com>
2025-04-28 12:14:37 +0900

    코드 수정중

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,12 @@
-if ('serviceWorker' in navigator) {
-navigator.serviceWorker.register('./service-worker.js')
-    .then(function(registration) {
-    console.log('Service Worker 등록 성공:', registration.scope);
-    })
-    .catch(function(error) {
-    console.log('Service Worker 등록 실패:', error);
-    });
-}
+self.addEventListener('install', (e) => {
+    console.log('서비스워커 설치 완료');
+    self.skipWaiting();
+  });
+  
+  self.addEventListener('activate', (e) => {
+    console.log('서비스워커 활성화 완료');
+  });
+  
+  self.addEventListener('fetch', (e) => {
+    // 일단은 요청을 그냥 보내기만 해.
+});
